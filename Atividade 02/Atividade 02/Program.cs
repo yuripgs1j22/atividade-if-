1274624c39dea8ca5d88116a9d7fbdc935f80c9e
Yuri54978216 <Yuri54978216@edu.df.senac.br>
2025-04-24 08:30:18 -0300

    fazendo atividade que o professo pediu

---
+++ Atividade 02/Atividade 02/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int idade = 20;
+        bool temCarteira = true;
+        if (idade >= 18 && temCarteira)
+        {
+            Console.WriteLine("Pode dirigir");
+        }
+    }
+}
---
+++ atividade 01/atividade 01/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int numero = 25;
+        if (numero < 10 || numero > 20)
+        {
+            Console.WriteLine("Fora do intervalo");
+        }
+    }
+}
