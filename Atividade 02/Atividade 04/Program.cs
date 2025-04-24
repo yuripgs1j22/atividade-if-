45065486f4dfafdee0b5e7ee08ec36a884936309
Yuri54978216 <Yuri54978216@edu.df.senac.br>
2025-04-24 08:42:28 -0300

    atividade 04

---
+++ Atividade 02/Atividade 04/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int idade = 66;
+        double renda = 1900.0;
+        if (idade > 65 || renda < 2000)
+        {
+            Console.WriteLine("Está isento");
+        }
+    }
+}
--- Atividade 02/Atividade 04/obj/Debug/net7.0/Atividade 04.AssemblyInfo.cs
+++ Atividade 02/Atividade 04/obj/Debug/net7.0/Atividade 04.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("atividade 01")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Atividade 04")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("atividade 01")]
-[assembly: System.Reflection.AssemblyTitleAttribute("atividade 01")]
+[assembly: System.Reflection.AssemblyProductAttribute("Atividade 04")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Atividade 04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
