3d9f30579041963452b641c276be4c1294de9b74
Yuri54978216 <Yuri54978216@edu.df.senac.br>
2025-04-24 08:46:45 -0300

    atividade 05

---
+++ Atividade 02/atividade 05/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        bool ehClienteVip = false;
+        int compras = 12;
+        if (!ehClienteVip && compras > 10)
+        {
+            Console.WriteLine("Ganha desconto especial");
+        }
+    }
+}
