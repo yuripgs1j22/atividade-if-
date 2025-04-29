5cf28b65bca2578624a255279b07265bc31cccdd
Yuri54978216 <Yuri54978216@edu.df.senac.br>
2025-04-29 11:22:10 -0300

    faznedo atividade bem feita

---
+++ atividade nova/atividade nova/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int contador = 0;
+
+        while (contador <= 10)
+        {
+            Console.WriteLine(contador);
+            contador = contador + 1;
+        }
+    }
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
