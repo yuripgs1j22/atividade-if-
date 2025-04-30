c9c08476d3b1dcb4bae311049891f02d6ecc951b
Yuri54978216 <Yuri54978216@edu.df.senac.br>
2025-04-30 08:50:54 -0300

    atividade 07

---
+++ atividade nova/Atividade 07/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int count = 10;
+        while (count >= 1)
+        {
+            Console.WriteLine(count);
+            count--;
+        }
+
+        count = 1;
+        while (count <= 10)
+        {
+            Console.WriteLine(count);
+            count++;
+        }
+    }
+}
