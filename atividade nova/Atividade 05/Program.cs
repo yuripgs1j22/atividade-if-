b5b9a39a0c98505f05ea6578efd28b4eb51f9aa9
Yuri54978216 <Yuri54978216@edu.df.senac.br>
2025-04-30 08:46:13 -0300

    atividade 05

---
+++ atividade nova/Atividade 05/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int limite = int.Parse(Console.ReadLine());
+        int count = 0;
+        while (count <= limite)
+        {
+            Console.WriteLine(count);
+            count += 3;
+        }
+    }
+}
